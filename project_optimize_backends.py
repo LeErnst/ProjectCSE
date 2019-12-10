399a136afacdd3cd4cc5bdc739619ed6d33cd3cc
Lewin Ernst <lewin.ernst@uni-ulm.de>
2019-12-10 10:39:34 +0100

    Optimize Framework hinzugefügt

---
+++ project_optimize_backends.py
@@ -0,0 +1,249 @@
+import math
+import random
+import numpy
+from scipy.optimize import minimize
+from scipy.optimize import OptimizeResult
+from ..core.log import BaseLogger
+from pyrateoptics.optimize.optimize_backends import Backend
+
+class ProjectScipyBackend(Backend):
+    def __init__(self, optimize_func, options={}, **kwargs):
+        self.optimize_func = optimize_func
+        self.options = options
+        self.kwargs = kwargs
+
+    def run(self, x0):
+        res = minimize(self.func, x0=x0, args=(), method=self.optimize_func,
+                       options=self.options, **self.kwargs)
+        return res.x
+
+# from:
+# https://github.com/scipy/scipy/blob/v1.3.3/scipy/optimize/optimize.py#L77-L132
+"""
+class OptimizeResult(dict):
+    Represents the optimization result.
+
+    Attributes
+    ----------
+    x : ndarray
+        The solution of the optimization.
+    success : bool
+        Whether or not the optimizer exited successfully.
+    status : int
+        Termination status of the optimizer. Its value depends on the
+        underlying solver. Refer to `message` for details.
+    message : str
+        Description of the cause of the termination.
+    fun, jac, hess: ndarray
+        Values of objective function, its Jacobian and its Hessian (if
+        available). The Hessians may be approximations, see the documentation
+        of the function in question.
+    hess_inv : object
+        Inverse of the objective function's Hessian; may be an approximation.
+        Not available for all solvers. The type of this attribute may be
+        either np.ndarray or scipy.sparse.linalg.LinearOperator.
+    nfev, njev, nhev : int
+        Number of evaluations of the objective functions and of its
+        Jacobian and Hessian.
+    nit : int
+        Number of iterations performed by the optimizer.
+    maxcv : float
+        The maximum constraint violation.
+
+    Notes
+    -----
+    There may be additional attributes not listed above depending of the
+    specific solver. Since this class is essentially a subclass of dict
+    with attribute accessors, one can see which attributes are available
+    using the `keys()` method.
+    
+"""
+# from 
+# https://github.com/scipy/scipy/blob/v1.3.3/scipy/optimize/_minimize.py#L42-L626
+"""
+def minimize(fun, x0, args=(), method=None, jac=None, hess=None,
+             hessp=None, bounds=None, constraints=(), tol=None,
+             callback=None, options=None):
+    .
+    .
+    .
+    **Custom minimizers**
+    It may be useful to pass a custom minimization method, for example
+    when using a frontend to this method such as `scipy.optimize.basinhopping`
+    or a different library.  You can simply pass a callable as the ``method``
+    parameter.
+    
+    The callable is called as 
+    
+    ``method(fun, x0, args, **kwargs, **options)``
+    
+    where ``kwargs`` corresponds to any other parameters passed to `minimize`
+    (such as `callback`, `hess`, etc.), except the `options` dict, which has
+    its contents also passed as `method` parameters pair by pair.  Also, if
+    `jac` has been passed as a bool type, `jac` and `fun` are mangled so that
+    `fun` returns just the function values and `jac` is converted to a function
+    returning the Jacobian.  The method shall return an `OptimizeResult`
+    object.
+    The provided `method` callable must be able to accept (and possibly ignore)
+    arbitrary parameters; the set of parameters accepted by `minimize` may
+    expand in future versions and then these parameters will be passed to
+    the method.  You can find an example in the scipy.optimize tutorial.
+    .
+    .
+    .
+"""
+
+# custom optimize signature example from:
+# https://docs.scipy.org/doc/scipy/reference/tutorial/optimize.html
+"""
+def custmin(fun, x0, args=(), maxfev=None, stepsize=0.1,
+            maxiter=100, callback=None, **options):
+"""
+# another from:
+# https://github.com/scipy/scipy/blob/v1.3.3/scipy/optimize/optimize.py#L77-L132
+"""
+def _minimize_neldermead(func, x0, args=(), callback=None,
+                         maxiter=None, maxfev=None, disp=False,
+                         return_all=False, initial_simplex=None,
+                         xatol=1e-4, fatol=1e-4, adaptive=False,
+                         **unknown_options):
+"""
+# How they take the boundaries into account.
+# from
+# class FloatOptimizableVariable(OptimizableVariable):
+"""
+ import math
+
+ def left_bounded(x):
+     return math.fabs(left)*math.log((x - left)/math.fabs(left))
+
+ def left_bounded_inv(x):
+     return left + math.fabs(left)*math.exp(x/math.fabs(left))
+
+ def right_bounded(x):
+     return -math.fabs(right)*math.log((right - x)/math.fabs(right))
+
+ def right_bounded_inv(x):
+     return right - math.fabs(right)*math.exp(-x/math.fabs(right))
+
+ def both_bounded(x):
+     return math.log((-x + left)/(x - right)) * math.fabs(left - right)
+
+ def both_bounded_inv(x):
+     return left +\
+             (right - left)/(1. + math.exp(-x/math.fabs(right - left)))
+
+
+"""                                                                          
+    
+
+def vanilla_sgd(func, x0, args=(), 
+                maxiter=250, disp=False, return_all=False, 
+                fatol=1e-4, stepsize=1e-3, **unknown_options):
+    """
+    maxiter: int
+        Maximum allowed number of iterations.
+    disp : bool
+        Set to True to print convergence messages.
+    fatol : number
+        Absolute error in func(xopt) between iterations that is acceptable for
+        convergence.
+    stepsize: number
+        Size of step between iterations.
+    """
+    xopt = x0
+    path = [x0]
+    iterNum = 0
+    iterTolf = fatol+1
+
+    while ((iterNum >= maxIt) and (iterTolf >= fatol)):
+        xopt = xopt-eta*gradx
+        path.append(xopt)
+        iterNum += 1
+        iterTolf = numpy.absolute(func(path[-1])-func(path[-2]))
+
+
+def test_minimize_neldermead(func, x0, args=(), 
+                             maxiter=100, 
+                             xatol=1e-6, fatol=1e-6,
+                             **unknown_options):
+    alpha = 1
+    beta = 2
+    gamma = 0.5
+    sigma = 0.5
+    # convert x into float array and flat
+    x0 = numpy.asfarray(x0).flatten()
+    N = len(x0)
+    nonzdelt = 0.05 # params from scipy impl
+    zdelt = 0.00025 # params from scipy impl
+    # set up simplex, also from scipy impl in order to get the same init simplex
+    sim = numpy.zeros((N + 1, N), dtype=x0.dtype)
+    sim[0] = x0
+    for k in range(N):
+        y = numpy.array(x0, copy=True)
+        if y[k] != 0:
+            y[k] = (1 + nonzdelt)*y[k]
+        else:
+            y[k] = zdelt
+        sim[k + 1] = y
+
+    # initilize f simplex
+    fsim = numpy.zeros((N + 1,), float)
+    for k in range(N + 1):
+        fsim[k] = func(sim[k])
+
+    ind = numpy.argsort(fsim)
+    # sort f simplex so fsim [0,:] is the lowest function value
+    fsim = numpy.take(fsim, ind, 0)
+    # sort so sim[0,:] has the lowest function value
+    sim = numpy.take(sim, ind, 0)
+    iterNum = 1
+    # we follow the implementation from the num3 lecture notes
+    while (iterNum < maxiter):
+        # same break condition as scipy
+        if (numpy.max(numpy.ravel(numpy.abs(sim[1:] - sim[0]))) <= xatol and
+                numpy.max(numpy.abs(fsim[0] - fsim[1:])) <= fatol):
+            break
+        xbar = numpy.add.reduce(sim[:-1],0)/N
+        x_a = xbar - alpha*(sim[-1]-xbar)
+        f_a = func(x_a)
+
+        if (fsim[0] <= f_a and f_a <= fsim[-2]):
+            sim[-1] = x_a
+        elif (f_a < fsim[0]):
+            x_ab = xbar - alpha*beta*(sim[-1]-xbar)
+            f_ab = func(x_ab)
+            if (f_ab < fsim[0]):
+                sim[-1] = x_ab
+            else: 
+                sim[-1] = x_a
+        else: # f_a > fsim[-2]
+            if (fsim[-2] < f_a and f_a < fsim[-1]):
+                x_ag = xbar - alpha*gamma*(sim[-1]-xbar)
+                f_ag = func(x_ag)
+                if (f_ag <= f_a):
+                    sim[-1] = x_ag
+                else: 
+                    for i in range(N):
+                        sim[i+1] = sigma*(sim[0]+sim[i+1])
+            else: # f_a is the worst value
+                xg = xbar + gamma*(sim[-1]-xbar)
+                fg = func(xg)
+                if (fg < fsim[-1]):
+                    sim[-1] = xg
+                else:
+                    for i in range(N):
+                        sim[i+1] = sigma*(sim[0]+sim[i+1])
+        for i in range(N+1):
+            fsim[i] = func(sim[i])
+
+        iterNum += 1
+        ind = numpy.argsort(fsim)
+        sim = numpy.take(sim, ind, 0)
+        fsim = numpy.take(fsim, ind, 0)
+
+    x_sol = sim[0]
+    fval = numpy.min(fsim)
+    return OptimizeResult(fun=fval, x=x_sol, nit=iterNum, 
+                          final_simplex=(sim, fsim))
+
