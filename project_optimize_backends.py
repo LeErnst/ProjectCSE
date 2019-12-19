0645fbcf7037c6f372f51674d8b1df9ff857b86d
Leandro Ebner <leandro.ebner@uni-ulm.de>
2019-12-19 14:08:03 +0100

    Kommentare geaendert

--- project_optimize_backends.py
+++ project_optimize_backends.py
@@ -154,125 +154,6 @@ class ProjectScipyBackend(Backend):
         self.res = res
         return res.x
 
-# from:
-# https://github.com/scipy/scipy/blob/v1.3.3/scipy/optimize/optimize.py#L77-L132
-"""
-class OptimizeResult(dict):
-    Represents the optimization result.
-
-    Attributes
-    ----------
-    x : ndarray
-        The solution of the optimization.
-    success : bool
-        Whether or not the optimizer exited successfully.
-    status : int
-        Termination status of the optimizer. Its value depends on the
-        underlying solver. Refer to `message` for details.
-    message : str
-        Description of the cause of the termination.
-    fun, jac, hess: ndarray
-        Values of objective function, its Jacobian and its Hessian (if
-        available). The Hessians may be approximations, see the documentation
-        of the function in question.
-    hess_inv : object
-        Inverse of the objective function's Hessian; may be an approximation.
-        Not available for all solvers. The type of this attribute may be
-        either np.ndarray or scipy.sparse.linalg.LinearOperator.
-    nfev, njev, nhev : int
-        Number of evaluations of the objective functions and of its
-        Jacobian and Hessian.
-    nit : int
-        Number of iterations performed by the optimizer.
-    maxcv : float
-        The maximum constraint violation.
-
-    Notes
-    -----
-    There may be additional attributes not listed above depending of the
-    specific solver. Since this class is essentially a subclass of dict
-    with attribute accessors, one can see which attributes are available
-    using the `keys()` method.
-    
-"""
-# from 
-# https://github.com/scipy/scipy/blob/v1.3.3/scipy/optimize/_minimize.py#L42-L626
-"""
-def minimize(fun, x0, args=(), method=None, jac=None, hess=None,
-             hessp=None, bounds=None, constraints=(), tol=None,
-             callback=None, options=None):
-    .
-    .
-    .
-    **Custom minimizers**
-    It may be useful to pass a custom minimization method, for example
-    when using a frontend to this method such as `scipy.optimize.basinhopping`
-    or a different library.  You can simply pass a callable as the ``method``
-    parameter.
-    
-    The callable is called as 
-    
-    ``method(fun, x0, args, **kwargs, **options)``
-    
-    where ``kwargs`` corresponds to any other parameters passed to `minimize`
-    (such as `callback`, `hess`, etc.), except the `options` dict, which has
-    its contents also passed as `method` parameters pair by pair.  Also, if
-    `jac` has been passed as a bool type, `jac` and `fun` are mangled so that
-    `fun` returns just the function values and `jac` is converted to a function
-    returning the Jacobian.  The method shall return an `OptimizeResult`
-    object.
-    The provided `method` callable must be able to accept (and possibly ignore)
-    arbitrary parameters; the set of parameters accepted by `minimize` may
-    expand in future versions and then these parameters will be passed to
-    the method.  You can find an example in the scipy.optimize tutorial.
-    .
-    .
-    .
-"""
-
-# custom optimize signature example from:
-# https://docs.scipy.org/doc/scipy/reference/tutorial/optimize.html
-"""
-def custmin(fun, x0, args=(), maxfev=None, stepsize=0.1,
-            maxiter=100, callback=None, **options):
-"""
-# another from:
-# https://github.com/scipy/scipy/blob/v1.3.3/scipy/optimize/optimize.py#L77-L132
-"""
-def _minimize_neldermead(func, x0, args=(), callback=None,
-                         maxiter=None, maxfev=None, disp=False,
-                         return_all=False, initial_simplex=None,
-                         xatol=1e-4, fatol=1e-4, adaptive=False,
-                         **unknown_options):
-"""
-# How they take the boundaries into account.
-# from
-# class FloatOptimizableVariable(OptimizableVariable):
-"""
- import math
-
- def left_bounded(x):
-     return math.fabs(left)*math.log((x - left)/math.fabs(left))
-
- def left_bounded_inv(x):
-     return left + math.fabs(left)*math.exp(x/math.fabs(left))
-
- def right_bounded(x):
-     return -math.fabs(right)*math.log((right - x)/math.fabs(right))
-
- def right_bounded_inv(x):
-     return right - math.fabs(right)*math.exp(-x/math.fabs(right))
-
- def both_bounded(x):
-     return math.log((-x + left)/(x - right)) * math.fabs(left - right)
-
- def both_bounded_inv(x):
-     return left +\
-             (right - left)/(1. + math.exp(-x/math.fabs(right - left)))
-
-
-"""                                                                          
-    
 
 def sgd(func, x0, args, 
         gradient=None, maxiter=250, fatol=1e-4,
@@ -410,4 +291,122 @@ def test_minimize_neldermead(func, x0, args=(),
     fval = numpy.min(fsim)
     return OptimizeResult(fun=fval, x=x_sol, nit=iterNum, 
                           final_simplex=(sim, fsim))
+# from:
+# https://github.com/scipy/scipy/blob/v1.3.3/scipy/optimize/optimize.py#L77-L132
+"""
+class OptimizeResult(dict):
+    Represents the optimization result.
 
+    Attributes
+    ----------
+    x : ndarray
+        The solution of the optimization.
+    success : bool
+        Whether or not the optimizer exited successfully.
+    status : int
+        Termination status of the optimizer. Its value depends on the
+        underlying solver. Refer to `message` for details.
+    message : str
+        Description of the cause of the termination.
+    fun, jac, hess: ndarray
+        Values of objective function, its Jacobian and its Hessian (if
+        available). The Hessians may be approximations, see the documentation
+        of the function in question.
+    hess_inv : object
+        Inverse of the objective function's Hessian; may be an approximation.
+        Not available for all solvers. The type of this attribute may be
+        either np.ndarray or scipy.sparse.linalg.LinearOperator.
+    nfev, njev, nhev : int
+        Number of evaluations of the objective functions and of its
+        Jacobian and Hessian.
+    nit : int
+        Number of iterations performed by the optimizer.
+    maxcv : float
+        The maximum constraint violation.
+
+    Notes
+    -----
+    There may be additional attributes not listed above depending of the
+    specific solver. Since this class is essentially a subclass of dict
+    with attribute accessors, one can see which attributes are available
+    using the `keys()` method.
+    
+"""
+# from 
+# https://github.com/scipy/scipy/blob/v1.3.3/scipy/optimize/_minimize.py#L42-L626
+"""
+def minimize(fun, x0, args=(), method=None, jac=None, hess=None,
+             hessp=None, bounds=None, constraints=(), tol=None,
+             callback=None, options=None):
+    .
+    .
+    .
+    **Custom minimizers**
+    It may be useful to pass a custom minimization method, for example
+    when using a frontend to this method such as `scipy.optimize.basinhopping`
+    or a different library.  You can simply pass a callable as the ``method``
+    parameter.
+    
+    The callable is called as 
+    
+    ``method(fun, x0, args, **kwargs, **options)``
+    
+    where ``kwargs`` corresponds to any other parameters passed to `minimize`
+    (such as `callback`, `hess`, etc.), except the `options` dict, which has
+    its contents also passed as `method` parameters pair by pair.  Also, if
+    `jac` has been passed as a bool type, `jac` and `fun` are mangled so that
+    `fun` returns just the function values and `jac` is converted to a function
+    returning the Jacobian.  The method shall return an `OptimizeResult`
+    object.
+    The provided `method` callable must be able to accept (and possibly ignore)
+    arbitrary parameters; the set of parameters accepted by `minimize` may
+    expand in future versions and then these parameters will be passed to
+    the method.  You can find an example in the scipy.optimize tutorial.
+    .
+    .
+    .
+"""
+
+# custom optimize signature example from:
+# https://docs.scipy.org/doc/scipy/reference/tutorial/optimize.html
+"""
+def custmin(fun, x0, args=(), maxfev=None, stepsize=0.1,
+            maxiter=100, callback=None, **options):
+"""
+# another from:
+# https://github.com/scipy/scipy/blob/v1.3.3/scipy/optimize/optimize.py#L77-L132
+"""
+def _minimize_neldermead(func, x0, args=(), callback=None,
+                         maxiter=None, maxfev=None, disp=False,
+                         return_all=False, initial_simplex=None,
+                         xatol=1e-4, fatol=1e-4, adaptive=False,
+                         **unknown_options):
+"""
+# How they take the boundaries into account.
+# from
+# class FloatOptimizableVariable(OptimizableVariable):
+"""
+ import math
+
+ def left_bounded(x):
+     return math.fabs(left)*math.log((x - left)/math.fabs(left))
+
+ def left_bounded_inv(x):
+     return left + math.fabs(left)*math.exp(x/math.fabs(left))
+
+ def right_bounded(x):
+     return -math.fabs(right)*math.log((right - x)/math.fabs(right))
+
+ def right_bounded_inv(x):
+     return right - math.fabs(right)*math.exp(-x/math.fabs(right))
+
+ def both_bounded(x):
+     return math.log((-x + left)/(x - right)) * math.fabs(left - right)
+
+ def both_bounded_inv(x):
+     return left +\
+             (right - left)/(1. + math.exp(-x/math.fabs(right - left)))
+
+
+"""                                                                          
+ 
