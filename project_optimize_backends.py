d168160227419633bf8f475cdcaef33600fbb687
michael weinstein <michael.weinstein@uni-ulm.de>
2020-01-09 12:37:28 +0100

    PSO_NM algo hinzugefuegt

--- project_optimize_backends.py
+++ project_optimize_backends.py
@@ -651,6 +651,348 @@ def test_minimize_neldermead(func, x0, args=(),
     fval = numpy.min(fsim)
     return OptimizeResult(fun=fval, x=x_sol, nit=iterNum, 
                           final_simplex=(sim, fsim))
+
+
+def PSO_NM(func,x0,args=(),N=None,vel_max=None,maxiter=50,\
+                 c0=None,c1=1.4,c2=1.4,**unknown_options):
+    # N = population size (has to be: N >= 2n+1; with n=problem size)
+    # vel_max = maximal velocity of a particle
+    # c0,c1 and c2 are variables for updating the velocity of the particles
+
+    #rm is a method which brings a solution x in a infeasible region ...
+    #to a feasible region
+    #---------------------------------------------------------------------------
+    def rm(x,lb,ub):                      # repair method (later grad.bas.r.m)
+        # moves positions on bounds if they don't be in the feasible region
+        from copy import deepcopy
+        x_neu = deepcopy(x)
+        for t in range(len(x)):
+            if (x[t]<lb[t]):
+                x_neu[t] = lb[t]
+            if (x[t]>ub[t]):
+                x_neu[t] = ub[t]
+        return x_neu
+
+    def Cf(x,lb,ub):                        # Constraint fitness priority-based ranking method
+        C = numpy.empty(len(lb)+len(ub))
+        g = numpy.empty(len(lb)+len(ub))
+        for t in range(len(x)):
+            g[2*t] = lb[t]-x[t]             # value of g_i(x)
+            g[2*t+1] = x[t]-ub[t]
+        gmax = numpy.amax(g)
+        for t in range(len(g)):
+            if (g[t]<=0):
+                C[t] = 1
+            else:
+                C[t] = 1-(g[t]/gmax)
+        weight = 1/float((len(C)))
+        Cf = 0
+        for t in range(len(C)):
+            Cf += weight*C[t]
+        return Cf
+
+    def nelder_mead_con(initial_simplex,func,Cf,lb,ub,maxiter=50,tolError=1e-8):
+    # Nelder-Mead for constraint optimization
+    # initial_simplex should be array of shape (N+1,N) with N: problem size
+        alpha = 1
+        beta = 0.5
+        gamma = 2
+        delta = 0.5
+        tol = 1e-12
+
+        n = len(initial_simplex[0])
+        xfC = []                    # list with [[x1,f(x1),Cf(x1)],[x2,f(x2),Cf(x2)],...
+        for t in range(n+1):
+            xfC.append([initial_simplex[t],func(initial_simplex[t]),\
+                        Cf(initial_simplex[t],lb,ub)])
+        
+        # termination condition for first check in while loop:
+        f_mean = 0
+        for t in range(n+1):
+            f_mean += xfC[t][1]
+        f_mean = f_mean * (1/(float(n)+1))
+        error = 0
+        for t in range(n+1):
+            error += (xfC[t][1]-f_mean)**2
+        error *= 1/(float(n)+1)
+
+        it = 1
+        while (it <= maxiter and error > tolError):
+            # sort xfC from low to high f(x) values
+            xfC = sorted(xfC,key=lambda elem: elem[1])
+            # Reflection:
+            # Determine x_cent without x_high:
+            x_cent = numpy.zeros(n)
+            for t in range(n):
+                for j in range(n):
+                    x_cent[j] += xfC[t][0][j]
+            for t in range(n):
+                x_cent[t] /= n
+            # make reflection:
+            x_high = xfC[n][0]
+            f_high = xfC[n][1]
+            x_refl = (1+alpha)*x_cent-alpha*x_high
+            Cf_refl = Cf(x_refl,lb,ub)
+            f_refl = func(x_refl)
+            x_low = xfC[0][0]
+            Cf_low = xfC[0][2]
+            f_low = xfC[0][1]
+            
+    
+            if ((Cf_refl<1 and Cf_refl>Cf_low) or ((Cf_refl<=1+tol and Cf_refl>=1-tol) and \
+                f_refl<f_low)):
+                # first expansion case:
+                if (Cf_refl<1 and Cf_refl>Cf_low):
+                    x_exp = gamma*x_refl+(1-gamma)*x_cent
+                    f_exp = func(x_exp)
+                    Cf_exp = Cf(x_exp,lb,ub)
+                    if (Cf_exp>Cf_low or (Cf_exp>=1-tol and Cf_exp<=1+tol)):
+                        # expansion accepted -> replace x_high by x_exp:
+                        xfC[n][0] = x_exp
+                        xfC[n][1] = f_exp
+                        xfC[n][2] = Cf_exp
+                    else:
+                        # replace x_high by x_refl:
+                        xfC[n][0] = x_refl
+                        xfC[n][1] = f_refl
+                        xfC[n][2] = Cf_refl
+                # second expansion case:
+                if ((Cf_refl>=1-tol and Cf_refl<=1+tol)and(f_refl<f_low)):
+                    x_exp = gamma*x_refl+(1-gamma)*x_cent
+                    f_exp = func(x_exp)
+                    Cf_exp = Cf(x_exp,lb,ub)
+                    if ((Cf_exp>=1-tol and Cf_exp<=1+tol)and(f_exp<f_low)):
+                        # expansion ist acceptet, replace x_high by x_exp:
+                        xfC[n][0] = x_exp
+                        xfC[n][1] = f_exp
+                        xfC[n][2] = Cf_exp
+                    else:
+                        # replacing x_high by x_refl:
+                        xfC[n][0] = x_refl
+                        xfC[n][1] = f_refl
+                        xfC[n][2] = Cf_refl
+    
+            else:
+                # first contraction case:
+                if (Cf_refl<1 and Cf_refl<=Cf_low):
+                    x_cont = beta*x_high+(1-beta)*x_cent
+                    f_cont = func(x_cont)
+                    Cf_cont = Cf(x_cont,lb,ub)
+                    if ((Cf_cont>Cf_low)or(Cf_cont>=1-tol and Cf_cont<=1+tol)):
+                        # contraction is accepted: replace x_high by x_cont:
+                        xfC[n][0] = x_cont
+                        xfC[n][1] = f_cont
+                        xfC[n][2] = Cf_cont
+                    else:
+                        # shrinkage attempts to all points except x_low:
+                        for t in range(n):
+                            xfC[1+t][0] *= delta
+                            xfC[1+t][0] += (1-delta)*x_low
+                # second contraction case:
+                if((Cf_refl>=1-tol and Cf_refl<=1+tol)and(f_refl>=f_low) \
+                    and (f_refl<=f_high)):
+                    # replace x_high by x_refl:
+                    xfC[n][0] = x_refl
+                    xfC[n][1] = f_refl
+                    xfC[n][2] = Cf_refl
+                    x_cont = beta*x_refl+(1-beta)*x_cent
+                    f_cont = func(x_cont)
+                    Cf_cont = Cf(x_cont,lb,ub)
+                    if((Cf_cont>=1-tol and Cf_cont<=1+tol)and(f_cont<f_low)):
+                        # contraction accepted: replace x_high by x_cont
+                        xfC[n][0] = x_cont
+                        xfC[n][1] = f_cont
+                        xfC[n][2] = Cf_cont
+                    else:
+                        # shrinkage the entire simplex but not x_low
+                        for t in range(n):
+                            xfC[1+t][0] *= delta
+                            xfC[1+t][0] += (1-delta)*x_low
+                # third contraction case:
+                if((Cf_refl>=1-tol and Cf_refl<=1+tol)and(f_refl>=f_low) \
+                    and (f_refl>f_high)):
+                    x_cont = beta*x_high+(1-beta)*x_cent
+                    f_cont = func(x_cont)
+                    Cf_cont = Cf(x_cont,lb,ub)
+                    if((Cf_cont>=1-tol and Cf_cont<=1+tol)and(f_cont<f_low)):
+                        # contraction accepted: replace x_high by x_cont
+                        xfC[n][0] = x_cont
+                        xfC[n][1] = f_cont
+                        xfC[n][2] = Cf_cont
+                    else:
+                        # shrinkage the entire simplex but not x_low
+                        for t in range(n):
+                            xfC[1+t][0] *= delta
+                            xfC[1+t][0] += (1-delta)*x_low
+            it = it+1
+            # termination condition:
+            f_mean = 0
+            for t in range(n+1):
+                f_mean += xfC[t][1]
+            f_mean *= 1/(float(n)+1)
+            error = 0
+            for t in range(n+1):
+                error += (xfC[t][1]-f_mean)**2
+            error *= 1/(float(n)+1)   
+
+        final_simplex = numpy.empty([n+1,n])
+        for t in range(n+1):
+            final_simplex[t] = xfC[t][0]
+        return final_simplex
+    
+    #---------------------------------------------------------------------------
+
+    n = len(x0)             # problem size
+    bounds = unknown_options['bounds']
+
+    if (N==None):           # define swarm size
+        N = 21*n+1
+    if (vel_max==None):     # define maximal velocity (30% of feasible interval)
+        vel_max = numpy.empty(n)
+        for q in range(n):
+            vel_max[q] = 0.1 * (bounds.ub[q]-bounds.lb[q])
+    if (c0==None):          # define weight c0
+        c0 = 0.5 + (random.random()/3)
+
+    bounds = unknown_options['bounds']
+    # Generate a class for the particles:
+    class particle:
+        #best_swarm = numpy.empty(n)
+        #best_swarm_f = 0
+        def __init__(self,pos,vel):
+            self.pos = pos
+            self.pos_f = func(pos)
+            #self.best_part = pos        # best position of this particle
+            #self.best_part_f = self.pos_f
+            self.vel = vel
+        def updatePos(self,pos):
+            self.pos = pos
+            self.pos_f = func(self.pos)
+            #self.checkBestPart
+        def update(self,c0,c1,c2,gBest,nBest):  # gBest:global best; nBest:neighborhood best
+            r1 = random.random()       # random number between 0 and 1
+            r2 = random.random()
+            self.vel = c0*self.vel + c1*r1*(nBest-self.pos) + \
+                        c2*r2*(gBest-self.pos)
+            #check if self.vel is out of [-vel_max,vel_max]:
+            for q in range(n):
+                if (self.vel[q] < -vel_max[q]):
+                    self.vel[q] = -vel_max[q]
+                if (self.vel[q] > vel_max[q]):
+                    self.vel[q] = vel_max[q]
+            # Update position
+            self.pos = self.pos + self.vel
+            self.pos_f = func(self.pos)
+            #self.checkBestPart
+        #def checkBestPart():            # checks if actual pos. ist better than best_part
+        #    if (self.pos_f <= self.best_part_f):
+        #        self.best_part = self.pos
+        #        self.best_part_f = func(self.pos)
+    
+    # Initialization of all particles with Position and velocity:
+    swarm = []
+    for i in range(N):
+        pos0 = numpy.random.uniform(bounds.lb,bounds.ub)    # start position in feasible region
+        vel0 = random.random() * vel_max                    # random start velocity
+        swarm.append(particle(pos0,vel0))
+
+    k = 0           # Iteration
+    xBest = x0
+    xBest_f = func(x0)
+    stopCrit = 0            # counter for stopping criteria
+
+    while (k <= maxiter):
+        test = 0
+        #Evaluate solutions and apply Repair Method if not in feasible region:
+        liste = []                            # list to safe (pos(i),f(pos(i)),...
+        for i in range(N):
+            # check if solution is in feasibale region, else apply Repair Method
+            if not (numpy.all(swarm[i].pos>=bounds.lb) and \
+                    numpy.all(swarm[i].pos<=bounds.ub)):
+                repairedPos = rm(swarm[i].pos,bounds.lb,bounds.ub)
+                test += 1
+                swarm[i].updatePos(repairedPos)
+            # write tupel (i,f(i)) in liste:
+            temp = [i,swarm[i].pos_f]
+            liste.append(temp)
+        # sort liste from low func values to high func values:
+        liste = sorted(liste,key=lambda elem: elem[1])
+        print("test = ",test)
+        print("best value = ",liste[0][1])
+        if numpy.all(swarm[liste[0][0]].pos>=bounds.lb) and numpy.all(swarm[liste[0][0]].pos<=bounds.ub):
+            print("bounds are good")
+        # Update xBest and xBest_f if solution is better:
+        if (liste[0][1] <= xBest_f):
+            xBest = swarm[liste[0][0]].pos
+            xBest_f = liste[0][1]
+            stopCrit = 0
+
+        stopCrit = stopCrit+1
+        print("Best = ",xBest_f)
+
+        # check stopping criteria:
+        if (k==maxiter or stopCrit>=5):
+            return OptimizeResult(fun=xBest_f, x=xBest, nit = k)
+            break
+
+        # Apply Nelder-Mead to the (n+1) best particles:
+        initial_simplex = numpy.empty([n+1,n])
+        for i in range(n+1):
+            initial_simplex[i] = swarm[liste[i][0]].pos
+        final_simplex = nelder_mead_con(initial_simplex,func,Cf,bounds.lb,bounds.ub)
+        # Update (n+1)th particle:
+        for i in range(n+1):
+            particle = liste[i][0]
+            swarm[particle].updatePos(final_simplex[i])
+            liste[i][1] = swarm[particle].pos_f
+
+        # Update liste:
+        liste = sorted(liste,key=lambda elem: elem[1])
+        # Apply PSO to all N particles:
+        #1.) Determine global best particle:
+        gBest = liste[0][0]             # index of global best particle
+        #2.) Divide 20n paritcles in two neighborhoods with 10N particles and
+        #    determine best of these tows:
+        firstN = numpy.arange(n+1,10*n+n+1,dtype=int)
+        secondN = numpy.arange(10*n+n+1,20*n+n+1,dtype=int)
+        random.shuffle(firstN)          # random shuffeling
+        random.shuffle(secondN)         # "
+        for i in range(len(firstN)):
+            first = firstN[i]
+            second = secondN[i]
+            nBest = 0
+            if (swarm[first].pos_f <= swarm[second].pos_f):
+                nBest = first
+            else:
+                nBest = second
+            swarm[first].update(c0,c1,c2,swarm[gBest].pos,swarm[nBest].pos)     # update velocity and position
+            swarm[second].update(c0,c1,c2,swarm[gBest].pos,swarm[nBest].pos)
+
+        k = k+1
+
+    """
+        # loop over all particles:
+        for i in range(N):
+            f_i_k = func(swarm[i].pos)
+            print("f = ",f_i_k)
+            if (f_i_k <= swarm[i].best_part_f): # update best position of particle
+                swarm[i].best_part_f = f_i_k
+                swarm[i].best_part   = swarm[i].pos
+            if (f_i_k <= best_swarm_f):         # update best position of swarm
+                best_swarm_f = f_i_k
+                best_swarm = swarm[i].pos
+
+        # loop over all particles for Updating:
+        for i in range(N):
+            swarm[i].update(c1,c2,best_swarm)
+        
+        # Increment Iteration
+        k = k+1
+
+    return OptimizeResult(fun=best_swarm_f, x=best_swarm, nit = k)
+    """
+
+
 # from:
 # https://github.com/scipy/scipy/blob/v1.3.3/scipy/optimize/optimize.py#L77-L132
 """
