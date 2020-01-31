c284190914ff8de9cb46136e30724a15a1724792
Lewin Ernst <lewin.ernst@uni-ulm.de>
2020-01-31 18:37:32 +0100

    nochmals die stochastic meritfunctionrms geändert und die ursprünglich standard meritfunctionrms wieder implementiert. ihr müsst nun mit standard1 bzw standard2 wählen welche meritfunctionrms ihr wollt

--- aux_merit_bundle.py
+++ aux_merit_bundle.py
@@ -7,6 +7,7 @@ import matplotlib
 import random
 import sys
 import copy
+import warnings
 
 # --- optical system and raytracing
 from pyrateoptics.raytracer.optical_system              import OpticalSystem
@@ -75,7 +76,7 @@ def buildInitialbundle(osa, s, sysseq, rays_list, numrays, wavelength):
     return initialbundle
 
 def get_bundle_merit(osa, s, sysseq, rays_dict, numrays=10,
-                     wavelength=[0.587e-3], whichmeritfunc='standard',
+                     wavelength=[0.587e-3], whichmeritfunc='standard1',
                      error='error2', sample_param='wave', penalty=True,
                      penaltyVerz=False,f=100.0):
     """
@@ -86,7 +87,10 @@ def get_bundle_merit(osa, s, sysseq, rays_dict, numrays=10,
 
     f: Brennweite
     """
-
+    if (whichmeritfunc=='sgd2' and sample_param=='wave'):
+        warnings.warn('This meritfunctionrms does not use the reference chief ray (green light) for error measurement!\n')
+    if (whichmeritfunc=='sgd2' and sample_param=='ray'):
+        raise ValueError('This definition of the meritfunctionrms is not available for this type of sample_param. Try sample_param == bundle.')
 
     initialbundle = buildInitialbundle(osa, s, sysseq, rays_dict, 
                                        numrays, wavelength)
@@ -103,13 +107,72 @@ def get_bundle_merit(osa, s, sysseq, rays_dict, numrays=10,
 
     # --- define meritfunctions:
     # You can add your meritfunctionsrms-implementation and then add it to 
-    # the dictionary. If the string whichmeritfunc is not a key in the dict. the 
+    # the dictionary. If the string whichmeritfunc is not a key in the dict. the
     # standard_error2 function is taken. (exception handling)
     # This is a necessary generalization, because for instance the sgd needs 
     # a special meritfunc.
 
     # ---------------standard meritfunction
-    def meritfunctionrms_standard(my_s, **kwargs):
+    def meritfunctionrms_standard1(my_s, **kwargs):
+        """
+        Standard meritfunctionrms: 
+        """
+        res = 0
+
+        # Loop over all bundles
+        for i in range(0, numbundles):
+            x = np.array([])
+            y = np.array([])
+
+            # Loop over wavelenghts
+            for j in range(0, numwaves):
+                my_initialbundle = copy.copy(initialbundle[i][j])
+                rpaths = my_s.seqtrace(my_initialbundle, sysseq)
+
+                # put x and y value for ALL wavelengths in x and y array 
+                # to caculate mean
+                x = np.append(x, rpaths[0].raybundles[-1].x[-1, 0, :])
+                y = np.append(y, rpaths[0].raybundles[-1].x[-1, 1, :])
+
+                # penalize overlapping lenses:
+                SubInt = len(rpaths[0].raybundles)-4
+                diff = np.zeros([numrays_true,SubInt])    
+                for w in range(SubInt):
+                    for t in range(len(rpaths[0].raybundles[w+3].x[0,2])):
+                        if math.isnan(rpaths[0].raybundles[w+3].x[-1,2,t]):
+                            pass
+                        else:
+                            diff[t,w] = rpaths[0].raybundles[w+3].x[-1,2,t] -\
+                                        rpaths[0].raybundles[w+3].x[0,2,t]
+
+                #check for overlapping lenses (negative value in diff):
+                overlap = 0.0
+                for t in range(len(diff)):
+                    for w in range(len(diff[0])):
+                        if (diff[t,w] < 0.0):
+                            overlap += abs(diff[t,w])
+
+                # penalty
+                res += (overlap+1.0)**(6) - 1.0
+                
+            xmean = np.mean(x)
+            ymean = np.mean(y)
+
+            # penalty for less rays
+            if (penalty == True):
+                res += (math.exp(numrays_waves/(len(x)+1e-1))-\
+                        math.exp(numrays_waves/(numrays_waves+1e-1)))
+
+            # Choose error function
+            if (error == 'error2'):
+                res += error2squared(x,xmean,y,ymean)
+            elif (error == 'error1'):
+                res += error1(x,xmean,y,ymean)
+
+        return res
+
+
+    def meritfunctionrms_standard2(my_s, **kwargs):
         """
         Standard meritfunctionrms: 
         """
@@ -150,11 +213,11 @@ def get_bundle_merit(osa, s, sysseq, rays_dict, numrays=10,
                             overlap += abs(diff[t,w])
                 res += (overlap+1.0)**(6) - 1.0          # wie gewichten?
                 
-                if (j==0):              # Verzeichnung is only referenced to reference wavelength
-                    if penaltyVerz:     # is actual only working for collimated bundles
-                        # check Verzeichnung: should be smaller than 1%
+                if (j==0): # distortion is only referenced to reference wavelength
+                    if penaltyVerz: # is only working for collimated bundles
+                        # check distortion: should be smaller than 1%
                         angle = rays_dict[i]["anglex"]
-                        SBH = f * math.tan(angle)       # Sollbildhoehe in x Richtung
+                        SBH = f * math.tan(angle) # Sollbildhoehe in x Richtung
                         if (len(rpaths[0].raybundles[-1].x[-1,1]) > 0):    # wenn hauptstrahl ueberhaupt auf bild ankommt, dann
                             Y = rpaths[0].raybundles[-1].x[-1,1,0]          # y Komponente des Hauptstrahls auf Bild
                             if Y >= SBH + 0.01*abs(SBH) or Y <= SBH - 0.01*abs(SBH):
@@ -168,7 +231,7 @@ def get_bundle_merit(osa, s, sysseq, rays_dict, numrays=10,
                     if (len(rpaths[0].raybundles[-1].x[-1,1])>0):            
                         yChief = np.append(yChief, rpaths[0].raybundles[-1].x[-1,1,0])
 
-            # this could be simplified, for the sake of clarity we do not
+            # penalty for less rays
             if (penalty == True):
                 res += (math.exp(numrays_waves/(len(x)+1e-1))-\
                         math.exp(numrays_waves/(numrays_waves+1e-1)))
@@ -185,7 +248,7 @@ def get_bundle_merit(osa, s, sysseq, rays_dict, numrays=10,
     # ---------------sgd meritfunction
     def meritfunctionrms_sgd1(my_s, **kwargs):
         if (len(kwargs) == 0):
-            return meritfunctionrms_standard(my_s)
+            return meritfunctionrms_standard1(my_s)
         else:
             res = 0
             # the stochastic gradient function has to tell this function which 
@@ -231,32 +294,39 @@ def get_bundle_merit(osa, s, sysseq, rays_dict, numrays=10,
                 x = np.append(x, rpaths[0].raybundles[-1].x[-1, 0, :])
                 y = np.append(y, rpaths[0].raybundles[-1].x[-1, 1, :])
  
-                # Add up all the mean values of the different wavelengths
-                xmean = np.mean(x)
-                ymean = np.mean(y)
-
                 # if the sample parameter is ray only one ray has been drawn
                 if (sample_param == 'ray'):
                     x = x[mask]
                     y = y[mask]
 
-                # TODO: this penalty can not be used with sample_param=ray
-                # this could be simplified, for the sake of clarity we do not
-                if (penalty == True):
+            # penalty for less rays
+            if (penalty == True):
+                if (sample_param == 'bundle'):
+                    res += (math.exp(numrays_waves/(len(x)+1e-1))-\
+                            math.exp(numrays_waves/(numrays_waves+1e-1)))
+                if (sample_param == 'wave'):
                     res += (math.exp(numrays_true/(len(x)+1e-1))-\
                             math.exp(numrays_true/(numrays_true+1e-1)))
+                if (sample_param == 'ray'):
+                    res += (math.exp(1/(len(x)+1e-2))-\
+                            math.exp(1/(1+1e-2)))
 
-                # Choose error function
-                if (error == 'error2'):
-                    res += error2squared(x, xmean, y, ymean)
-                elif (error == 'error1'):
-                    res += error1(x, xmean, y, ymean)
+            # Add up all the mean values of the different wavelengths
+            xmean = np.mean(x)
+            ymean = np.mean(y)
+
+            # Choose error function
+            if (error == 'error2'):
+                res += error2squared(x, xmean, y, ymean)
+            elif (error == 'error1'):
+                res += error1(x, xmean, y, ymean)
 
             return res
 
+
     def meritfunctionrms_sgd2(my_s, **kwargs):
         if (len(kwargs) == 0):
-            return meritfunctionrms_standard(my_s)
+            return meritfunctionrms_standard2(my_s)
         else:
             res = 0
             # the stochastic gradient function has to tell this function which 
@@ -271,23 +341,9 @@ def get_bundle_merit(osa, s, sysseq, rays_dict, numrays=10,
             if (sample_param == 'wave'):
                 sample_initialbundle = [initialbundle[sample_num//numwaves]\
                                                      [sample_num%numwaves]]
-            if (sample_param == 'ray'):
-                # TODO:
-                # this option can cause an error, because the number of rays 
-                # numrays is predetermined and therefore can not be changed 
-                # during an optimization run but it can happen that not all rays
-                # hit the image plane. Because of that a random number can be 
-                # drawn but there is no associated ray. the mask, which has then
-                # the wrong size will cause an error.
-                bundlenum = sample_num//(numwaves*numrays_true)
-                wavenum   = (sample_num-numwaves*numrays_true*bundlenum)//\
-                            numrays_true
-                raynum    = sample_num-numwaves*numrays_true*bundlenum-\
-                            numrays_true*wavenum
-
-                sample_initialbundle = [initialbundle[bundlenum][wavenum]]
-                mask = np.zeros(numrays_true, dtype=bool)
-                mask[raynum] = True
+                raise ValueError('This definition of the meritfunctionrms is \
+                                  not available for this type of sample_param.\
+                                  Try sample_param == bundle.')
 
             # Loop over sample_initialbundle
             x = np.array([])
@@ -316,51 +372,70 @@ def get_bundle_merit(osa, s, sysseq, rays_dict, numrays=10,
                         else:
                             diff[t,w] = rpaths[0].raybundles[w+3].x[-1,2,t] -\
                                         rpaths[0].raybundles[w+3].x[0,2,t]
+
                 #check for overlapping lenses (negative value in diff):
                 overlap = 0.0
                 for t in range(len(diff)):
                     for w in range(len(diff[0])):
                         if (diff[t,w] < 0.0):
                             overlap += abs(diff[t,w])
-                res += (overlap+1.0)**(6) - 1.0          # wie gewichten?
-                
-                if (j==0):              # Verzeichnung is only referenced to reference wavelength
-                    if penaltyVerz:     # is actual only working for collimated bundles
-                        # check Verzeichnung: should be smaller than 1%
+
+                # penalty term
+                res += (overlap+1.0)**(6) - 1.0
+                # distortion for reference wavelength but only when all 
+                # wavelengths are considered
+                if (j==0 and sample_param == 'bundle'):
+                    if penaltyVerz: # only working for collimated bundles
+                        # check distortion: should be smaller than 1%
                         angle = rays_dict[i]["anglex"]
-                        SBH = f * math.tan(angle)       # Sollbildhoehe in x Richtung
-                        if (len(rpaths[0].raybundles[-1].x[-1,1]) > 0):    # wenn hauptstrahl ueberhaupt auf bild ankommt, dann
-                            Y = rpaths[0].raybundles[-1].x[-1,1,0]          # y Komponente des Hauptstrahls auf Bild
-                            if Y >= SBH + 0.01*abs(SBH) or Y <= SBH - 0.01*abs(SBH):
-                                res += 1000*(abs(Y-SBH)-(0.01*abs(SBH)))**(2)                 # wie gewichten???
+                        SBH = f * math.tan(angle) # target image height (x dir)
+                        if (len(rpaths[0].raybundles[-1].x[-1,1]) > 0):
+                            Y = rpaths[0].raybundles[-1].x[-1,1,0]
+                            if (Y >= SBH + 0.01*abs(SBH) or \
+                                Y <= SBH - 0.01*abs(SBH)):
+                                res += 1000*(abs(Y-SBH)-(0.01*abs(SBH)))**(2)
 
-
+                # TODO: this is a little bit tricky with a sample_param != 
+                # bundle, because the drawn bundle doesnt have to be the green
+                # wavelength and therefore no reference chief ray is computed
                 # compute Chief ray of reference wavelength (green one):
-                if (j==0):                      # green w.l. should be in the front
+                if (j==0):# green wavelength should be the first entry
                     if (len(rpaths[0].raybundles[-1].x[-1,0])>0):
-                        xChief = np.append(xChief, rpaths[0].raybundles[-1].x[-1,0,0])
+                        xChief = \
+                           np.append(xChief, rpaths[0].raybundles[-1].x[-1,0,0])
                     if (len(rpaths[0].raybundles[-1].x[-1,1])>0):            
-                        yChief = np.append(yChief, rpaths[0].raybundles[-1].x[-1,1,0])
+                        yChief = \
+                           np.append(yChief, rpaths[0].raybundles[-1].x[-1,1,0])
 
-                # this could be simplified, for the sake of clarity we do not
-                if (penalty == True):
+            # penalty for less rays
+            if (penalty == True):
+                if (sample_param == 'bundle'):
                     res += (math.exp(numrays_waves/(len(x)+1e-1))-\
                             math.exp(numrays_waves/(numrays_waves+1e-1)))
-                # Choose error function
-                if (error == 'error2'):
-                    if (len(xChief)>0 and len(yChief)>0):
-                        res += error2squared(x,xChief,y,yChief)
-                elif (error == 'error1'):
-                    if (len(xChief)>0 and len(yChief)>0):
-                        res += error1(x,xChief,y,yChief)
+                if (sample_param == 'wave'):
+                    res += (math.exp(numrays_true/(len(x)+1e-1))-\
+                            math.exp(numrays_true/(numrays_true+1e-1)))
+
+            # Add up all the mean values of the different wavelengths
+            xmean = np.mean(x)
+            ymean = np.mean(y)
+
+            # Choose error function
+            if (error == 'error2'):
+                if (len(xChief)>0 and len(yChief)>0):
+                    res += error2squared(x,xChief,y,yChief)
+            elif (error == 'error1'):
+                if (len(xChief)>0 and len(yChief)>0):
+                    res += error1(x,xChief,y,yChief)
 
             return res
 
     # for defining which meritfunction should be used
-    switcher = {'standard' : meritfunctionrms_standard,
-                'sgd1'     : meritfunctionrms_sgd1   ,
-                'sgd2'     : meritfunctionrms_sgd2     }
+    switcher = {'standard1' : meritfunctionrms_standard1,
+                'standard2' : meritfunctionrms_standard2,
+                'sgd1'      : meritfunctionrms_sgd1,
+                'sgd2'      : meritfunctionrms_sgd2}
 
     return (initialbundle, switcher.get(whichmeritfunc, \
-                                        meritfunctionrms_standard))
+                                        meritfunctionrms_standard1))
 
